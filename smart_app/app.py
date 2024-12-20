9b3cc5e63963e01f742230e0cd581fc82b90d7ad
nasywa3032 <nasywa3032@gmail.com>
2024-12-20 18:03:08 +0700

    mengubah root

--- smart_app/app.py
+++ smart_app/app.py
@@ -3,7 +3,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Mengambil data dari form
